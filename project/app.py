069de07790a5018616f41a3913aa557310a3144b
Benjamin-Daza-Jimenez <benja.daza.jimenez@gmail.com>
2026-02-04 17:08:48 -0300

    fix subida manual con casos none, nan y nat

--- project/app.py
+++ project/app.py
@@ -189,11 +189,15 @@ if st.session_state.df is None:
 
             df = data.duracion(df, 'Fecha_Inicio', 'Fecha_Fin', 'Duracion')
             st.write("Generando tópicos con BERTopic, esto puede tardar varios minutos...")
+
             columnas = ['Resumen', 'Descripcion', 'Causa', 'Solucion']
 
             for col in columnas:
                 df[col] = df[col].fillna("Sin Información").astype(str).str.strip()
                 df[col] = df[col].replace("", "Sin Información")
+                df[col] = df[col].replace("nan", "Sin Información")
+                df[col] = df[col].replace("NaT", "Sin Información")
+                df[col] = df[col].replace("None", "Sin Información")
 
             df = bertopic.bertopic_app(df.copy(), columnas)
             df = data.clean(df, COLUMNS_RENAMED, 'Fecha_Inicio')
